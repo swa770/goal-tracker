d0279fce9fb435c521adbd4c9a54892e93bd1e76
Su Yong Won <dsbrianwon@gmail.com>
2021-01-05 18:02:44 +0900

    Initial Commit

---
+++ GoalTracker/GoalTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalTrackerApp.swift
+//  GoalTracker
+//
+//  Created by Suyong Won on 1/5/21.
+//
+
+import SwiftUI
+
+@main
+struct GoalTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
